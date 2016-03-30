92921bf33d80ae2b7eb88e99a098c6ca1d62d565
EnAsn <enasnjhchen@gmail.com>
2016-03-30 20:25:56 +0800

    Show the list of attributes

--- discorgs/restore.js
+++ discorgs/restore.js
@@ -5,13 +5,13 @@ fs.readFile('./myData.json', 'utf8', function (err, data) {
 	} else {
 		var obj = JSON.parse(data);
 		console.log(obj.length);
-		/*
+		
 		   for (i = 0; i < obj.length; i++){
 			var row = obj[i];
 			for (var myKey in row){
 				console.log("key: " + myKey + ", value: " + row[myKey]);
 			}
 		}
-		*/
+		
 	}
 });
