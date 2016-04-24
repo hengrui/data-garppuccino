14be21f5b777b5904319f9f96a5c84709f47aad8
EnAsn <enasnjhchen@gmail.com>
2016-04-24 16:58:46 +0800

    Translate json to xml

--- discorgs/json2xml.js
+++ discorgs/json2xml.js
@@ -1,5 +1,25 @@
+/*
+ * In order to use RSwoosh, the xml should be in the below form:
+ *	<attribute name="some_name">
+ *		<value>
+ *			first value
+ *		</value>
+ *		<value>
+ *			second value
+ *		</value>
+ *		...
+ *	</attribute>
+ *
+ *	The original matchInternal function compares two attributes' 
+ *	values iteratively, which means once a pair of components equals
+ *	these two attribute would be treated as the same and the function
+ *	starts to compare remaining attributes.
+ */
+
+var fs = require('fs');
 var js2xmlparser = require('js2xmlparser');
-fs.readFile('./jsonData/myData_artist.json', 'utf8', function (err, data){
+var option = process.argv;
+fs.readFile(option[2], 'utf8', function (err, data){
 	if (err) {
 		console.log(err);
 	} else {
@@ -9,7 +29,14 @@ fs.readFile('./jsonData/myData_artist.json', 'utf8', function (err, data){
 		for (var i = 0; i < obj.length; i++){
 			output['artist'].push(obj[i]);
 		}
-		console.log(js2xmlparser('ArtistSet', output));
+		var xml_content = js2xmlparser('ArtistSet', output);
+		fs.writeFile(option[3], xml_content, 'utf8', function(err, data){
+			if (err){
+				console.log('Writing fails');
+			} else {
+				console.log('XML file has been generated');
+			}
+		});
 	}
 
 });
