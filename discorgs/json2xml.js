5d0a9043ff3da88fe0a7c0059ddd43a4d0ff2b20
EnAsn <enasnjhchen@gmail.com>
2016-04-25 16:15:48 +0800

    Modify output format

--- discorgs/json2xml.js
+++ discorgs/json2xml.js
@@ -25,24 +25,58 @@ fs.readFile(option[2], 'utf8', function (err, data){
 	} else {
 		var obj = JSON.parse(data);
 		var output = {};
-		output['artist'] = [];
-		
-		for (var i = 0; i < obj.length; i++){
+		output['artist'] = [];		
+		for (var i = 0; i < obj.length; i++){//obj.length; i++){
 			var row = obj[i];
 			var detail = {};
 			detail['attribute'] = [];
 			for (var myKey in row){
-				var tmp = {
-					"@": {
-						"name": myKey
-					},
-					"value": row[myKey]
-				};
+				var deRaw = row[myKey];
+				var type = typeof deRaw;
+				var	tmp;
+				if (type != 'object'){
+					tmp = {
+						"@": {
+							"name": myKey
+						},
+						"value": row[myKey]
+					}
+				} else {
+					var data_in_deRaw = [];
+					var flag = 0;
+					for (var derawKey in deRaw){
+						if (typeof deRaw[derawKey] == 'object'){
+							flag = 1;
+							//console.log('object');
+							//console.log(deRaw[derawKey]);
+							var complex_data = deRaw[derawKey];
+							for (var c_key in complex_data){
+								data_in_deRaw.push(complex_data[c_key]);
+							}
+						} else {
+							tmp = {
+								"@": {
+									"name": myKey
+								},
+								"value": row[myKey]
+							}
+						}
+					}
+					if (flag) {
+						tmp = {
+							"@": {
+								"name": myKey
+							},
+							"value": data_in_deRaw
+						}
+					}
+				}
 				detail['attribute'].push(tmp);
 			}
 			output['artist'].push(detail);
 		}
 		var xml_content = js2xmlparser('artistSet', output);
+		//console.log(xml_content);
 		fs.writeFile(option[3], xml_content, 'utf8', function(err, data){
 			if (err){
 				console.log('Writing fails');
