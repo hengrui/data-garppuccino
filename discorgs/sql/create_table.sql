9e511e76d1618281a10c737e03ec6e829093dc77
EnAsn <enasnjhchen@gmail.com>
2016-04-30 21:23:24 +0800

    Add json data into database

--- discorgs/sql/create_table.sql
+++ discorgs/sql/create_table.sql
@@ -1,60 +1,66 @@
 -- create tables for discogs
 
-create table if not exists  discogs_release (  
-rid int PRIMARY KEY, 
-styles varchar(255),                    
-videos text,   
-series varchar(20),   
-labels text,           
-community text,      
-year char(4),      
-images text,             
-format_quantity varchar(255), 
-genres varchar(255),                  
-thumb varchar(255),   
-extraartists text,         
-title varchar(255),
-artists text, 
-data_changed varchar(255), 
-master_id int, 
-tracklist text,
-status varchar(255), 
-released_formatted varchar(255), 
-estimated_weight varchar(255),
-released varchar(64),
-date_added varchar(64),
-country varchar(255),
-notes text,
-identifiers text,
-companies text,
-uri varchar(255),
-formats text,
-resource_url varchar(255),
-data_quality varchar(255)
+CREATE TABLE IF NOT EXISTS  discogs_release (  
+	rid varchar(20), 
+	styles text,       
+	videos text,   
+	series text,   
+	labels text,           
+	community text,      
+	year int,      
+	images text,             
+	format_quantity text, 
+	genres text,
+	thumb text,   
+	extraartists text,         
+	title text,
+	artists text, 
+	-- it's possible that several aritists work on one album
+	data_changed varchar(255), 
+	master_id varchar(20), 
+	-- this release could be a version of a master release
+	tracklist text,
+	status text, 
+	released_formatted text, 
+	estimated_weight text,
+	released text,
+	date_added varchar(64),
+	date_changed varchar(64),
+	country text,
+	notes text,
+	identifiers text,
+	companies text,
+	uri varchar(512),
+	formats text,
+	resource_url varchar(512),
+	master_url varchar(512),
+	data_quality text
 );
 
-create table if not exists discogs_artist (
-aid int PRIMARY KEY,
-profile text,
-urls text,
-release_url varchar(255),
-name varchar(255),
-uri varchar(255),
-images text,
-data_quality varchar(255),
-realname varchar(255),
-groups text,
-aliases text,
-namevariations text,
-release_id int
+CREATE TABLE IF NOT EXISTS discogs_artist (
+	aid varchar(20) PRIMARY KEY,
+	profile text,
+	urls text,
+	releases_url varchar(512),
+	resource_url varchar(512),
+	name varchar(255),
+	uri varchar(512),
+	images text,
+	data_quality varchar(255),
+	realname varchar(255),
+	groups text,
+	aliases text,
+	namevariations text
 );
 
-create table if not exists discogs_track (
-tid SERIAL PRIMARY KEY,
-duration varchar(20),
-position varchar(3),
-type_ varchar(100),
-titile varchar(255),
-release_id int
+CREATE TABLE IF NOT EXISTS discogs_track (
+	tid SERIAL PRIMARY KEY,
+	-- the primary key is auto_increased
+	duration varchar(20),
+	position varchar(3),
+	type_ varchar(100),
+	titile varchar(255),
+	release_id varchar(20)
+	-- indicates rid in discogs_release
 );
 
--- discorgs/sql/psql.js
+++ discorgs/sql/psql.js
@@ -1,41 +1,66 @@
 var pg = require('pg');
-var conString = "postgres://jack:bigdata@localhost/EnAsn";
+var conString = "postgres://jack:bigdata@localhost:5432/EnAsn";
 var client = new pg.Client(conString);
 var fs = require('fs');
-var squel = require('../lastfm/crawler/squel');
+var squel = require('../../lastfm/crawler/squel');
+var option = process.argv;
+var filename = option[2];
+var tablename = option[3];
+var sql_insert = [];
+var sql_num = 0;
+var async = require('async');
+var actual_id;
+var obj;
 
 client.connect(function(err) {
 	if (err) {
 		return console.error('could not connect to postgres', err);
 	}
 	
-	fs.readFile('./myData.json', 'utf8', function (err, data){
+	fs.readFile(filename, 'utf8', function (err, data){
 		if (err) {
 			console.log(err);
 		} else {
-			var obj = JSON.parse(data);
-			console.log(obj);
-			for (i = 0; i < 1; i++){
-				var sql = squel.insert().into('discorgs');
+			switch (tablename){
+				case 'discogs_artist':
+					actual_id = 'aid';
+				case 'discogs_release':
+					actual_id = 'rid';
+			}
+			obj = JSON.parse(data);
+			//console.log(obj);
+			for (i = 0; i < obj.length; i++){
+				var sql = squel.insert().into(tablename);
 				var row = obj[i];
-				console.log(row);
+				var content = {};
+				//console.log(row);
 				for (var myKey in row){
 					//sql.set(myKey, JSON.stringify(row[myKey]));
-					row[myKey] = JSON.stringify(row[myKey]);
+					if (myKey == 'id'){
+						content[actual_id] = JSON.stringify(row[myKey]);
+					} else {
+						content[myKey] = JSON.stringify(row[myKey]);
+					}
 				}
-				sql.setFields(row);
-				console.log(sql.toString());
-				client.query(sql.toString(), function(err){
-					if (err){
-						console.log(err);
-					} 
-					client.end();
-				});
+				sql.setFields(content);
+				sql_insert[sql_num] = sql.toString();
+				sql_num++;
 			}
+			console.log(sql_num);
+			var counter = -1;
+			async.eachSeries(sql_insert, function(sql_insert, callback){
+				counter++;
+				console.log(counter);
+				client.query(sql_insert, function(err, res){
+					callback(err, res);
+				});
+			}, function(err){
+				client.end();
+				console.log('err: ' + err);
+			});
 		}
 	});
 });
-
 
 
 
